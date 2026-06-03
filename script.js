fa75135f35df59e9c6aafe640abe8cfeaea448ed
nw-n <nyuwa1221@gmail.com>
2026-06-04 01:43:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const songs = [
+  "TRAIN",
+  "No More Cry",
+  "Shake body",
+  "Secret Express",
+  "pani pani",
+  "POLICE MEN",
+  "Drive on week",
+  "FLASHBACK",
+  "Bloody Night",
+  "Make it hot!"
+];
 
+function getRandomSong() {
+  return songs[Math.floor(Math.random() * songs.length)];
+}
+
+const buttons = document.querySelectorAll(".song-button");
+
+buttons[0].textContent = getRandomSong();
+buttons[1].textContent = getRandomSong();
